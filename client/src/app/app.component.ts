e6cd74a20e567ef09f8ade0eb1900a16a5acaac0
mel-than-nae <mel.than.nae@gmail.com>
2020-12-30 13:35:47 +0200

    client side redis

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 import { IProduct } from './shared/models/product';
+import { BasketService } from './basket/basket.service';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +11,17 @@ import { IProduct } from './shared/models/product';
 export class AppComponent implements OnInit{
   title = 'Skinet';
 
-  constructor() {
-
+  constructor(private basketService: BasketService) {
   }
 
   ngOnInit(): void {
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) {
+      this.basketService.getBasket(basketId).subscribe(() => {
+        console.log('initialized basket');
+      }, error => {
+        console.log(error);
+      });
+    }
   }
 }
--- client/src/app/core/nav-bar/nav-bar.component.ts
+++ client/src/app/core/nav-bar/nav-bar.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ShopService } from 'src/app/shop/shop.service';
 import { IProduct } from 'src/app/shared/models/product';
+import { BasketService } from 'src/app/basket/basket.service';
+import { Observable } from 'rxjs';
+import { IBasket } from 'src/app/shared/models/basket';
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,8 +12,10 @@ import { IProduct } from 'src/app/shared/models/product';
 })
 export class NavBarComponent implements OnInit {
   product: IProduct;
-  constructor(private shopService: ShopService) { }
+  basket$: Observable<IBasket>;
+  constructor(private shopService: ShopService, private basketService: BasketService) { }
 
   ngOnInit(): void {
+    this.basket$ = this.basketService.basket$;
   }
 }
