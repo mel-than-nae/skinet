84487a934ec85fd99ac18825e76ae890e25644ac
mel-than-nae <mel.than.nae@gmail.com>
2021-01-01 21:08:05 +0200

    end of section 17

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 import { IProduct } from './shared/models/product';
 import { BasketService } from './basket/basket.service';
+import { AccountService } from './account/account.service';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +12,24 @@ import { BasketService } from './basket/basket.service';
 export class AppComponent implements OnInit{
   title = 'Skinet';
 
-  constructor(private basketService: BasketService) {
+  constructor(private basketService: BasketService, private accountService: AccountService) {
   }
 
   ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+
+  loadCurrentUser() {
+  const token = localStorage.getItem('token');
+    this.accountService.loadCurrentUser(token).subscribe(() => {
+      console.log('loaded user');
+    }, error => {
+      console.log(error);
+    })
+  };
+
+  loadBasket() {
     const basketId = localStorage.getItem('basket_id');
     if (basketId) {
       this.basketService.getBasket(basketId).subscribe(() => {
--- client/src/app/core/nav-bar/nav-bar.component.ts
+++ client/src/app/core/nav-bar/nav-bar.component.ts
@@ -4,6 +4,8 @@ import { IProduct } from 'src/app/shared/models/product';
 import { BasketService } from 'src/app/basket/basket.service';
 import { Observable } from 'rxjs';
 import { IBasket } from 'src/app/shared/models/basket';
+import { IUser } from 'src/app/shared/models/user';
+import { AccountService } from 'src/app/account/account.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -11,11 +13,17 @@ import { IBasket } from 'src/app/shared/models/basket';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
-  product: IProduct;
   basket$: Observable<IBasket>;
-  constructor(private shopService: ShopService, private basketService: BasketService) { }
+  currentUser$: Observable<IUser>;
+
+  constructor(private basketService: BasketService, private accountService: AccountService) { }
 
   ngOnInit(): void {
     this.basket$ = this.basketService.basket$;
+    this.currentUser$ = this.accountService.currentUser$;
+  }
+
+  logout(){
+    this.accountService.logout();
   }
 }
